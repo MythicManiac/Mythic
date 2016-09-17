a92243a562faaaa479387a0d3adda32e28b5ab61
Mythic <mythicmaniac@gmail.com>
2016-09-17 19:08:05 +0300

    Initial commit

---
+++ Mythic.Utils/CsvUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Mythic.Utils
+{
+    public class CsvUtils
+    {
+        public static string[][] ReadCsv(string path)
+        {
+            var result = new List<string[]>();
+            var csvDatum = File.ReadAllLines(path);
+            foreach(var line in csvDatum)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                result.Add(line.Split(','));
+            }
+            return result.ToArray();
+        }
+
+        public static void WriteCsv(string path, object[][] data)
+        {
+            var buffer = new MemoryStream();
+            var writer = new StreamWriter(buffer);
+            foreach(var entry in data)
+            {
+                writer.WriteLine(string.Join(",", entry));
+            }
+            writer.Flush();
+            File.WriteAllBytes(path, buffer.ToArray());
+        }
+    }
+}
---
+++ Mythic.UtilsTests/Mythic.UtilsTests/CsvUtilsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mythic.Utils;
+using System.IO;
+
+namespace Mythic.UtilsTests
+{
+    [TestClass]
+    public class CsvUtilsTests
+    {
+        [TestMethod]
+        public void TestReadCsv()
+        {
+            var data = CsvUtils.ReadCsv("Data/TestCsv.csv");
+            Assert.AreEqual("firstColumn", data[0][0]);
+            Assert.AreEqual("secondColumn", data[0][1]);
+            Assert.AreEqual("thirdColumn", data[0][2]);
+            Assert.AreEqual("25", data[1][0]);
+            Assert.AreEqual("39", data[1][1]);
+            Assert.AreEqual("50", data[1][2]);
+            Assert.AreEqual("12", data[2][0]);
+            Assert.AreEqual("30", data[2][1]);
+            Assert.AreEqual("53", data[2][2]);
+        }
+
+        [TestMethod]
+        public void TestWriteCsv()
+        {
+            var testFilePath = "TestCsv.csv";
+            if (File.Exists(testFilePath))
+                File.Delete(testFilePath);
+
+            var data = new object[][]
+            {
+                new object[] {"kek", 43, 2 },
+                new object[] {true, 23.3f, .4d }
+            };
+            CsvUtils.WriteCsv(testFilePath, data);
+            var read = CsvUtils.ReadCsv(testFilePath);
+            Assert.AreEqual("kek", read[0][0]);
+            Assert.AreEqual("43", read[0][1]);
+            Assert.AreEqual("2", read[0][2]);
+            Assert.AreEqual("True", read[1][0]);
+            Assert.AreEqual("23.3", read[1][1]);
+            Assert.AreEqual("0.4", read[1][2]);
+        }
+    }
+}
