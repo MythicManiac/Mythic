6df505a6feb0e8e47a4013fa4699c0e8377ab9d4
Mythic <mythicmaniac@gmail.com>
2016-09-20 20:57:04 +0300

    Add new and refactor old CSV utilities
    Rename most CSV utilities
    Add a conversion utility
    Add a read + convert utility
    Add a data list pair read utility
    Add tests

--- Mythic.Utils/CsvUtils.cs
+++ Mythic.Utils/CsvUtils.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Mythic.Utils
 {
     public class CsvUtils
     {
-        public static string[][] ReadCsv(string path)
+        public static string[][] Read(string path)
         {
             var result = new List<string[]>();
             var csvDatum = File.ReadAllLines(path);
-            foreach(var line in csvDatum)
+            foreach (var line in csvDatum)
             {
-                if(string.IsNullOrWhiteSpace(line))
+                if (string.IsNullOrWhiteSpace(line))
                     continue;
 
                 result.Add(line.Split(','));
@@ -19,16 +20,57 @@ namespace Mythic.Utils
             return result.ToArray();
         }
 
-        public static void WriteCsv(string path, object[][] data)
+        public static void Write<T>(string path, T[][] data)
         {
             var buffer = new MemoryStream();
             var writer = new StreamWriter(buffer);
-            foreach(var entry in data)
+            foreach (var entry in data)
             {
                 writer.WriteLine(string.Join(",", entry));
             }
             writer.Flush();
             File.WriteAllBytes(path, buffer.ToArray());
         }
+
+        public static T[][] ConvertValues<T>(string[][] values)
+        {
+            var result = new T[values.Length][];
+            for (var i = 0; i < values.Length; i++)
+            {
+                result[i] = new T[values[i].Length];
+                for (var j = 0; j < values[i].Length; j++)
+                {
+                    result[i][j] = (T)Convert.ChangeType(values[i][j], typeof(T));
+                }
+            }
+            return result;
+        }
+
+        public static T[][] ReadAndConvert<T>(string path)
+        {
+            var values = Read(path);
+            return ConvertValues<T>(values);
+        }
+
+        public static Tuple<T1[], T2[]>[] ReadDataListPair<T1, T2>(string file1, string file2)
+        {
+            var items1 = ReadAndConvert<T1>(file1);
+            var items2 = ReadAndConvert<T2>(file2);
+
+            if (items1.Length != items2.Length)
+                throw new DataListPairLengthException();
+
+            var result = new Tuple<T1[], T2[]>[items1.Length];
+            for(var i = 0; i < items1.Length; i++)
+            {
+                result[i] = Tuple.Create(items1[i], items2[i]);
+            }
+            return result;
+        }
+    }
+
+    public class DataListPairLengthException : Exception
+    {
+        public DataListPairLengthException() : base("Data list pairs length don't match") { }
     }
 }
--- Mythic.UtilsTests/Mythic.UtilsTests/CsvUtilsTests.cs
+++ Mythic.UtilsTests/Mythic.UtilsTests/CsvUtilsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mythic.Utils;
 using System.IO;
 
@@ -8,10 +7,12 @@ namespace Mythic.UtilsTests
     [TestClass]
     public class CsvUtilsTests
     {
+        private const string TEST_FILE_PATH = "TestCsv.csv";
+
         [TestMethod]
-        public void TestReadCsv()
+        public void TestRead()
         {
-            var data = CsvUtils.ReadCsv("Data/TestCsv.csv");
+            var data = CsvUtils.Read("Data/TestRead.csv");
             Assert.AreEqual("firstColumn", data[0][0]);
             Assert.AreEqual("secondColumn", data[0][1]);
             Assert.AreEqual("thirdColumn", data[0][2]);
@@ -24,25 +25,92 @@ namespace Mythic.UtilsTests
         }
 
         [TestMethod]
-        public void TestWriteCsv()
+        public void TestWrite()
         {
-            var testFilePath = "TestCsv.csv";
-            if (File.Exists(testFilePath))
-                File.Delete(testFilePath);
-
+            Clear(TEST_FILE_PATH);
             var data = new object[][]
             {
                 new object[] {"kek", 43, 2 },
                 new object[] {true, 23.3f, .4d }
             };
-            CsvUtils.WriteCsv(testFilePath, data);
-            var read = CsvUtils.ReadCsv(testFilePath);
+            CsvUtils.Write(TEST_FILE_PATH, data);
+            var read = CsvUtils.Read(TEST_FILE_PATH);
             Assert.AreEqual("kek", read[0][0]);
             Assert.AreEqual("43", read[0][1]);
             Assert.AreEqual("2", read[0][2]);
             Assert.AreEqual("True", read[1][0]);
             Assert.AreEqual("23.3", read[1][1]);
             Assert.AreEqual("0.4", read[1][2]);
+        }
+
+        [TestMethod]
+        public void TestConvertValues()
+        {
+            Clear(TEST_FILE_PATH);
+            var data = new double[][]
+            {
+                new double[] { 2.4, 2.5, 7.3 },
+                new double[] { -2.3, -0.01, -6 }
+            };
+            CsvUtils.Write(TEST_FILE_PATH, data);
+            var read = CsvUtils.Read(TEST_FILE_PATH);
+            var converted = CsvUtils.ConvertValues<double>(read);
+            for(var i = 0; i < data.Length; i++)
+            {
+                CollectionAssert.AreEqual(data[i], converted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestReadAndConvert()
+        {
+            Clear(TEST_FILE_PATH);
+            var data = new double[][]
+            {
+                new double[] { 2.4, 2.5, 7.3 },
+                new double[] { -2.3, -0.01, -6 }
+            };
+            CsvUtils.Write(TEST_FILE_PATH, data);
+            var converted = CsvUtils.ReadAndConvert<double>(TEST_FILE_PATH);
+            for (var i = 0; i < data.Length; i++)
+            {
+                CollectionAssert.AreEqual(data[i], converted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestReadDataListPairSuccess()
+        {
+            var file1 = Clear("TestCsv1.csv");
+            var file2 = Clear("TestCsv2.csv");
+
+            var data1 = new double[][]
+            {
+                new double[] { 2.3, 93.2, -28 },
+                new double[] { 0.00001, -329.45, 9.9999 }
+            };
+            var data2 = new string[][]
+            {
+                new string[] { "Madness?", "...", "No..." },
+                new string[] { "This", "IS", "SPARTA!" }
+            };
+
+            CsvUtils.Write(file1, data1);
+            CsvUtils.Write(file2, data2);
+
+            var pairs = CsvUtils.ReadDataListPair<double, string>(file1, file2);
+            for(var i = 0; i < pairs.Length; i++)
+            {
+                CollectionAssert.AreEqual(data1[i], pairs[i].Item1);
+                CollectionAssert.AreEqual(data2[i], pairs[i].Item2);
+            }
+        }
+
+        private string Clear(string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+            return path;
         }
     }
 }
